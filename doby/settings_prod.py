14e754f088462f8da4f53468165da89800fe288d
garibov <garibov1569@gmail.com>
2026-05-09 18:52:49 +0300

    ci: change settings for ci/cd

--- doby/settings_prod.py
+++ doby/settings_prod.py
@@ -1,5 +1,6 @@
 from settings import *
 
+SECRET_KEY = Env.get_str('DJANGO_SECRET_KEY')
 DEBUG = False
 ALLOWED_HOSTS = Env.get_list('ALLOWED_HOSTS')
 CSRF_TRUSTED_ORIGINS = Env.get_list('CSRF_TRUSTED_ORIGINS')
